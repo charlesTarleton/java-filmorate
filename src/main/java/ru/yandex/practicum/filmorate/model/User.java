f6f9762aac10308129a8b3bd703e48192b12bcc0
Rustam N <charlsetarleton@yandex.ru>
2023-06-10 21:27:35 +0300

    Учтены комментарии первого ревью ТЗ11

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
+
 @lombok.Data
 public class User {
     private Long id;
